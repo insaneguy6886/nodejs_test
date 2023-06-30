159a7a7bb6be38a233f33a4fd17dfca548e44562
tienlehung1080 <tienlehung1080@gmail.com>
2023-06-30 10:30:38 +0700

    Add route input group

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newsRouter = require("./news");
 const siteRouter = require("./site");
+const inputGroupRouter = require("./inputgroup");
 
 function route(app) {
+  app.use("/inputgroup", inputGroupRouter);
   app.use("/news", newsRouter);
   app.use("/", siteRouter);
 
